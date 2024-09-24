70e6181b4737e7f3fc4a772f56e239314d633db0
mateuszwalo <162806862+mateuszwalo@users.noreply.github.com>
2024-09-24 19:01:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+import tensorflow as tf
 
-st.title('🎈 App Name')
+st.title("APKA")
 
-st.write('Hello world!')
+
