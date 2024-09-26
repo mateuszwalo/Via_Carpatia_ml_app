31a28673c6ab74cf2c6579ea68bf0fc0a479815f
mateuszwalo <162806862+mateuszwalo@users.noreply.github.com>
2024-09-26 16:31:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,10 @@ page_bg_img = '''
 <style>
 .stApp {
     background-image: url("https://raw.githubusercontent.com/mateuszwalo/Via_Carpatia_ml_app/master/73935a88-e3d1-42ce-8994-1fe5b286c80e.jpg");
-    background-size: cover;
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
 }
 </style>
 '''
