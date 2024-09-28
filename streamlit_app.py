a3df5e999e70a7ad4e289b4bb95b9c022f3f0c0c
mateuszwalo <162806862+mateuszwalo@users.noreply.github.com>
2024-09-28 07:41:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,7 @@ if st.button('🔮 Predict'):
 st.info(
     """
     **Author:** *Mateusz Walo*  
-    **Graphic desgin:** *Karolina Gajowiak*  
+    **Graphic design:** *Karolina Gajowiak*  
     **LinkedIn:** [My LinkedIn profile :)](https://www.linkedin.com/in/mateuszwalo/)   
     **Data:** [Mental Health Dataset](https://www.kaggle.com/datasets/bhavikjikadara/mental-health-dataset)  
     """
